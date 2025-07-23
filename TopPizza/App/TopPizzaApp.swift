9b7915239b9b7bb9037acf0000cd4ea39b59f58f
webdmitriev <webdmitriev@gmail.com>
2025-07-23 15:37:55 +0300

    Addead Colors, Data Images, Struct Clean Architecture

---
+++ TopPizza/App/TopPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopPizzaApp.swift
+//  TopPizza
+//
+//  Created by Олег Дмитриев on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TopPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ TopPizza/Presentation/HomeView/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TopPizza
+//
+//  Created by Олег Дмитриев on 23.07.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
